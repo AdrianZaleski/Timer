490f4c9046fd7c40d261c36ce107eb5ea8d2019a
Adrian <azuniasty@gmail.com>
2021-08-21 15:44:04 +0200

    clock-service.js function added

---
+++ js/clock-service.js
@@ -0,0 +1,15 @@
+class ClockService{
+    setDefaultTime(){
+        this.time = 3;
+    }
+
+    decreaseTime(){
+        if(this.time>0){
+        this.time --
+        }
+    }
+
+    getSeconds(){
+        return this.time
+    }
+}
